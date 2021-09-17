be58501051afb9e090c0d8c22cdddde8f5d6df1a
jclark10 <james.clark@tufts.edu>
2021-09-17 17:24:14 -0400

    using jsonify

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class MovieIdAccess(Resource):
             return {'data': 'ERROR: NO MOVIE WITH CURRENT ID'}, 404
         else:
             # return movie_info.to_json(), 200
-            movie_json = jsonify(movie_info.to_dict())
+            movie_json = jsonify(movie_info.to_json(orient='records'))
             return movie_json
 
 
